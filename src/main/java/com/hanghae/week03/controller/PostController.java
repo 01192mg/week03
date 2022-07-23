611852010369573d24f25dd279f959987f4ff70c
01192mg <01192mg@gmail.com>
2022-07-23 18:56:54 +0900

    feat: 전체 게시글 조회 API 추가

--- src/main/java/com/hanghae/week03/controller/PostController.java
+++ src/main/java/com/hanghae/week03/controller/PostController.java
@@ -4,15 +4,23 @@ import com.hanghae.week03.model.Post;
 import com.hanghae.week03.model.PostRepository;
 import com.hanghae.week03.model.PostRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class PostController {
 
     private final PostRepository postRepository;
+
+    @GetMapping("/api/posts")
+    public List<Post> getPosts() {
+        return postRepository.findAllByOrderByCreatedAtDesc();
+    }
 
     @PostMapping("/api/posts")
     public Post createPost(@RequestBody PostRequestDto requestDto) {
--- src/main/java/com/hanghae/week03/model/PostRepository.java
+++ src/main/java/com/hanghae/week03/model/PostRepository.java
@@ -2,5 +2,9 @@ package com.hanghae.week03.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByOrderByCreatedAtDesc();
 }
